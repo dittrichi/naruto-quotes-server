39dcd5276229a881e137129a892e43c60c8b2459
Marco Dittrichi <87870749+dittrichi@users.noreply.github.com>
2021-12-11 20:15:55 +0100

    Initial create

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import jsonify,request,Flask
 from flask_cors import CORS, cross_origin
 import pandas as pd
 
-NarutoApi = Flask(__name__)
-cors = CORS(NarutoApi)
-NarutoApi.config['CORS_HEADERS'] = 'Content-Type'
+QuotesAPI = Flask(__name__)
+cors = CORS(QuotesAPI)
+QuotesAPI.config['CORS_HEADERS'] = 'Content-Type'
 
 df = pd.read_csv('finalQuotes.csv', names=['Speakers', 'Quotes'])
 
@@ -17,9 +17,9 @@ def get_random_quote():
     return quote_obj
 
 @cross_origin()
-@NarutoApi.route('/', methods=['GET'])
+@QuotesAPI.route('/', methods=['GET'])
 def dashboard():
     return jsonify(get_random_quote())
 
 if __name__ == '__main__':
-    NarutoApi.run(host= '127.0.0.1',port=5000)
+    QuotesAPI.run(host= '127.0.0.1',port=5000)
